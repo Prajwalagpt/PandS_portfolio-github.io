f0e57c1d31bcfe1f1bc4a7ce8a0d607f02cd6438
Prajwalagpt <162415178+Prajwalagpt@users.noreply.github.com>
2025-12-26 12:28:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 gsap.registerPlugin(ScrollTrigger);
 
+const hero = document.querySelector(".hero");
+const content = document.querySelector(".content");
+
+const heroHeight = hero.offsetHeight;
+const contentHeight = content.offsetHeight;
+
 gsap.timeline({
   scrollTrigger: {
-    trigger: ".hero",
+    trigger: hero,
     start: "top top",
-    end: "+=500",
+    end: `+=${heroHeight + contentHeight}`,
     scrub: true,
-    pin: true
+    pin: true,
   }
 })
-.to(".hero", { scale: 0.5 });
+.to(".hero h2", { scale: 0.5, opacity: 0 })
+.to(".hero p", { y: -100, opacity: 0 }, "<");
